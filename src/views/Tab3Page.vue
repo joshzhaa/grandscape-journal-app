99c8c5f538e10f01ae939b607afe7844c70017e9
erinle <erinle@umich.edu>
2023-12-03 21:19:07 -0500

    added a map and geolocation

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -5,19 +5,64 @@
         <ion-title>Map</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <div>
+      <ion-button @click="getCurrentPosition">
+        Get Current Location
+      </ion-button>
+      <br>
+      <ion-text>Latitude: {{ loc.lat }}</ion-text>
+      <ion-text>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Longitude: {{ loc.long }}</ion-text>
+      <br>
 
-      <ExploreContainer name="Tab 3 page" />
-    </ion-content>
+    
+  </div>
+
+
+  <ion-icon :icon="pin" style="z-index: 2;
+    position: absolute;
+    left: 29%;
+    top: 58%;
+    margin: auto;"></ion-icon>
+  <ion-img
+          src="https://geology.com/world/world-map.gif"
+          style="width:58%"
+          alt="World map"
+        ></ion-img>
+
+  
+    
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+
+import { defineComponent, ref } from 'vue';
+import { Geolocation } from '@capacitor/geolocation';
+import { IonIcon } from '@ionic/vue';
+import { pin } from 'ionicons/icons';
+
+export default defineComponent({
+
+  components: { IonImg, IonIcon },
+  setup() {
+    const loc = ref<{
+      lat: null | number;
+      long: null | number;
+    }>({
+      lat: null,
+      long: null,
+    });
+
+    const getCurrentPosition = async () => {
+      const pos = await Geolocation.getCurrentPosition();
+      loc.value = {
+        lat: pos.coords.latitude,
+        long: pos.coords.longitude,
+      };
+    };
+    return { getCurrentPosition, loc, pin };
+  },
+});
 </script>
