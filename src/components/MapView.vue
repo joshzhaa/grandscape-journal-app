8585aa14cf9c87157c3324fb44266a0d32226246
Josh <joshzhaa@gmail.com>
2023-12-06 11:22:38 -0500

    polish tab icons

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -6,14 +6,28 @@
 </template>
 
 <script setup lang="ts">
-defineProps({
-  latitude: String,
-  longitude: String,
-});
+  import { onMounted } from 'vue';
+  const props = defineProps({
+    latitude: String,
+    longitude: String,
+  });
+// the conversion is (0, 0) = (670, 325) and (-166.75, 68.33) = (45, 600)
+// longitude: px = 3.74813 * lon + 670
+// latitude: px = 4.02459 * lat + 325
+onMounted(() => {
+  const pin = document.querySelector('#pin-image');
+  pin.style.left = 3.74813 * props.longitude + 670;
+  pin.style.bottom = 4.02459 * props.latitude + 325;
+})
 </script>
 
 <style scoped>
+#container {
+  position: relative;
+}
+
 #map-image {
+  position: relative;
   width: 100%;
   z-index: 1;
 }
@@ -22,7 +36,7 @@ defineProps({
   position: absolute;
   width: 20px;
   z-index: 2;
-  top: 100px;
-  left: 100px;
+  left: 356px;
+  bottom: 495px;
 }
 </style>
